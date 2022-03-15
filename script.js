9ecc3468a7ff86237f7acb542937474089c6aab4
ParkerHadley <parker.l.hadley@gmail.com>
2022-03-14 23:35:21 -0400

    Testing scrolling function

---
+++ script.js
@@ -0,0 +1,11 @@
+"use script"
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+
+        }
+    })
+});
